6ca078e9598c86c6c9719385ef42d713f1067283
Alexander Gorbunov <entgorbunov@gmail.com>
2024-02-25 16:58:05 +0500

    hashtable

--- src/datastructures/hashtable/Main.java
+++ src/datastructures/hashtable/Main.java
@@ -1,2 +1,24 @@
-package datastructures.hashtable;public class Main {
+package datastructures.hashtable;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] array1 = {1, 3, 5};
+        int[] array2 = {2, 4, 5};
+
+        System.out.println(itemInCommon(array1, array2));
+
+    }
+    public static boolean itemInCommon(int[] array1, int[] array2){
+        HashMap<Integer, Boolean> integerBooleanHashMap = new HashMap<>();
+        for (int i : array1) {
+            integerBooleanHashMap.put(i, true);
+        }
+        for (int j : array2) {
+            if(integerBooleanHashMap.get(j) != null) return true;
+        }
+        return false;
+    }
 }
