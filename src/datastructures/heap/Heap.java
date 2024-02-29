53c3972f3461aaba546bc1049ef8cd172aba8656
Alexander Gorbunov <entgorbunov@gmail.com>
2024-02-29 08:01:01 +0500

    новое

--- src/datastructures/heap/Heap.java
+++ src/datastructures/heap/Heap.java
@@ -34,8 +34,15 @@ public class Heap {
 
     public void insert(int value) {
         heap.add(value);
+        int current = heap.size() - 1;
 
+        while (current > 0 && heap.get(current) > heap.get(parent(current))) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
     }
+
+
 
 
 
